e4a6f7a468bfe668c458d7a1dbdd8cd8265549ff
VK4WEB <vk4web@gmail.com>
2017-12-28 22:25:54 +0000

    fixed more warnings from checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,8 @@ public class Greeter {
 
   /**
   *	This method sends greetings.
+  * @param
+  * @return
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
